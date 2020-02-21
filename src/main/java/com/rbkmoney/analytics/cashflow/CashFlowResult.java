500359ef4e12a60565a6d9175c0b51bad8906888
a.romanov <a.romanov@rbkmoney.com>
2020-02-21 15:59:55 +0300

    BJ-801: Cash flow computer

---
+++ src/main/java/com/rbkmoney/analytics/cashflow/CashFlowResult.java
@@ -0,0 +1,17 @@
+package com.rbkmoney.analytics.cashflow;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CashFlowResult {
+
+    private final long accountId;
+    private final long totalAmount;
+    private final long merchantAmount;
+    private final long guaranteeDeposit;
+    private final long systemFee;
+    private final long providerFee;
+    private final long externalFee;
+}
