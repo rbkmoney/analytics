e10c27c67163492eef24c0651784afda34047be1
k.struzhkin <k.struzhkin@rbkmoney.com>
2020-03-13 13:57:21 +0300

    Add cache and delete bad state to balance table insert

--- src/main/java/com/rbkmoney/analytics/service/HgClientService.java
+++ src/main/java/com/rbkmoney/analytics/service/HgClientService.java
@@ -1,13 +1,20 @@
 package com.rbkmoney.analytics.service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.rbkmoney.analytics.domain.InvoicePaymentWrapper;
+import com.rbkmoney.analytics.exception.PaymentInfoNotFoundException;
 import com.rbkmoney.analytics.exception.PaymentInfoRequestException;
 import com.rbkmoney.analytics.utils.EventRangeFactory;
 import com.rbkmoney.damsel.payment_processing.*;
-import com.rbkmoney.machinegun.eventsink.MachineEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.TException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiFunction;
 
 @Slf4j
 @Service
@@ -20,13 +27,42 @@ public class HgClientService {
     private final InvoicingSrv.Iface invoicingClient;
     private final EventRangeFactory eventRangeFactory;
 
-    public Invoice getInvoiceInfo(MachineEvent event) {
+    private Cache<String, InvoicePaymentWrapper> cache = Caffeine.newBuilder()
+            .expireAfterWrite(5, TimeUnit.MINUTES)
+            .maximumSize(2000)
+            .build();
+
+    public InvoicePaymentWrapper getInvoiceInfo(String invoiceId,
+                                                BiFunction<String, com.rbkmoney.damsel.payment_processing.Invoice, Optional<InvoicePayment>> findPaymentPredicate,
+                                                String id, long sequenceId) {
+        InvoicePaymentWrapper invoicePaymentWrapper = cache.getIfPresent(generateKey(invoiceId, sequenceId));
+        if (invoicePaymentWrapper == null) {
+            invoicePaymentWrapper = new InvoicePaymentWrapper();
+            com.rbkmoney.damsel.payment_processing.Invoice invoiceInfo = invokeHg(invoiceId, sequenceId);
+            if (invoiceInfo == null) {
+                throw new PaymentInfoNotFoundException("Not found invoice info in hg!");
+            }
+            invoicePaymentWrapper.setInvoice(invoiceInfo.getInvoice());
+            findPaymentPredicate.apply(id, invoiceInfo)
+                    .ifPresentOrElse(invoicePaymentWrapper::setInvoicePayment,
+                            () -> {
+                                throw new PaymentInfoNotFoundException("Not found payment in invoice!");
+                            });
+            cache.put(generateKey(invoiceId, sequenceId), invoicePaymentWrapper);
+        }
+        return invoicePaymentWrapper;
+    }
+
+    private String generateKey(String invoiceId, long sequenceId) {
+        return invoiceId + "_" + sequenceId;
+    }
+
+    private Invoice invokeHg(String invoiceId, long sequenceId) {
         try {
-            return invoicingClient.get(USER_INFO, event.getSourceId(), eventRangeFactory.create(event.getEventId()));
+            return invoicingClient.get(USER_INFO, invoiceId, eventRangeFactory.create(sequenceId));
         } catch (TException e) {
             log.error("Error when HgClientService getInvoiceInfo e: ", e);
             throw new PaymentInfoRequestException(e);
         }
     }
-
 }
