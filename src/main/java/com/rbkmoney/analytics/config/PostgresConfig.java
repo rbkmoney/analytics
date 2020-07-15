2bb24368bb98a01ca3d7b4209d94252355e55362
vitaxa <v.banin@rbkmoney.com>
2020-07-15 10:24:55 +0300

    fix jooq datasource

--- src/main/java/com/rbkmoney/analytics/config/PostgresConfig.java
+++ src/main/java/com/rbkmoney/analytics/config/PostgresConfig.java
@@ -4,15 +4,13 @@ import com.rbkmoney.analytics.config.properties.PostgresDbProperties;
 import lombok.RequiredArgsConstructor;
 import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
-import org.jooq.impl.DefaultDSLContext;
-import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 
 import javax.sql.DataSource;
 
@@ -23,8 +21,7 @@ public class PostgresConfig {
     private final PostgresDbProperties postgresDbProperties;
 
     @Bean
-    @Primary
-    public DataSource postgresDatasource() {
+    public PGSimpleDataSource postgresDatasource() {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
         dataSource.setUrl(postgresDbProperties.getUrl());
         dataSource.setUser(postgresDbProperties.getUser());
@@ -33,20 +30,35 @@ public class PostgresConfig {
         return dataSource;
     }
 
-//    @Bean
-//    public DataSourceConnectionProvider dataSourceConnectionProvider(DataSource postgresDatasource) {
-//        return new DataSourceConnectionProvider(new TransactionAwareDataSourceProxy(postgresDatasource));
-//    }
-//
-//    @Bean
-//    public DefaultDSLContext dsl(DataSourceConnectionProvider dataSourceConnectionProvider) {
-//        DefaultConfiguration jooqConfiguration = new DefaultConfiguration();
-//        jooqConfiguration.set(dataSourceConnectionProvider);
-//        jooqConfiguration
-//                .set(new DefaultExecuteListenerProvider());
-//
-//        return new DefaultDSLContext(jooqConfiguration);
-//    }
+    @Bean
+    public DataSourceConnectionProvider dataSourceConnectionProvider(TransactionAwareDataSourceProxy transactionAwareDataSourceProxy) {
+        return new DataSourceConnectionProvider(transactionAwareDataSourceProxy);
+    }
+
+    @Bean
+    public TransactionAwareDataSourceProxy transactionAwareDataSource(DataSource postgresDatasource) {
+        return new TransactionAwareDataSourceProxy(postgresDatasource);
+    }
+
+    @Bean
+    public DataSourceConnectionProvider connectionProvider(TransactionAwareDataSourceProxy transactionAwareDataSourceProxy) {
+        return new DataSourceConnectionProvider(transactionAwareDataSourceProxy);
+    }
+
+    @Bean
+    public DefaultConfiguration configuration(DataSourceConnectionProvider dataSourceConnectionProvider) {
+        DefaultConfiguration jooqConfiguration = new DefaultConfiguration();
+        jooqConfiguration.set(dataSourceConnectionProvider);
+
+        return jooqConfiguration;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(PGSimpleDataSource dataSource) {
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        return initializer;
+    }
 
     @Bean
     @Autowired
