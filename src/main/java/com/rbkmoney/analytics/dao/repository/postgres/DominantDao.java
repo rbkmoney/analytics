a5e7c3e651598c6e5a4a410afcb0d2dc5a68ef5e
vitaxa <v.banin@rbkmoney.com>
2020-10-15 11:55:33 +0300

    cleanup

--- src/main/java/com/rbkmoney/analytics/dao/repository/postgres/DominantDao.java
+++ src/main/java/com/rbkmoney/analytics/dao/repository/postgres/DominantDao.java
@@ -1,28 +1,19 @@
 package com.rbkmoney.analytics.dao.repository.postgres;
 
-import com.rbkmoney.analytics.domain.db.tables.pojos.Dominant;
-import com.rbkmoney.analytics.domain.db.tables.records.DominantRecord;
 import com.rbkmoney.dao.impl.AbstractGenericDao;
-import com.rbkmoney.mapper.RecordRowMapper;
 import org.jooq.Query;
 import org.jooq.impl.DSL;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
 import static com.rbkmoney.analytics.domain.db.Tables.CATEGORY;
-import static com.rbkmoney.analytics.domain.db.Tables.DOMINANT;
 
 @Component
 public class DominantDao extends AbstractGenericDao {
 
-    private final RowMapper<Dominant> dominantRowMapper;
-
     public DominantDao(DataSource postgresDatasource) {
-        super(postgresDatasource);
-        this.dominantRowMapper = new RecordRowMapper<>(DOMINANT, Dominant.class);
-    }
+        super(postgresDatasource); }
 
     public Long getLastVersion() {
         Query query = getDslContext().select(DSL.max(DSL.field("version_id"))).from(
